df12c3d0103c5482116641e2b8f8669f653866e0
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-05-17 11:37:35 +0800

    Design dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,32 +1,37 @@
 <x-layouts.app>
     <div class="flex">
-        <div class="w-1/5 min-h-screen p-6 border-2 border-black space-y-12">
-            <div class="space-y-2">
-                <div>
-                    <img src="https://picsum.photos/200" alt="profile image"
-                        class="object-fill w-24 h-24 rounded-full" />
-                </div>
-                <h1>{{ $user->name }}</h1>
+        <x-layouts.sidebar :user="$user"></x-layouts.sidebar>
+        <div class="flex-1">
+            <div class="border-b-2">
+                <x-navbar></x-navbar>
             </div>
 
-            <nav class="flex flex-col space-y-2">
-                <a href="#">Dashboard</a>
+            <div class="flex justify-between p-6">
+                <h3>My Expenses</h3>
+                <h3>Dashboard</h3>
+            </div>
 
-                <a href="#">User Management</a>
-                <div class="flex flex-col ml-3 space-y-2">
-                    <a href="#">Roles</a>
-                    <a href="#">Users</a>
+            <div class="flex p-2 px-12">
+                <div class="flex-1">
+                    <div class="w-1/2 mx-auto">
+                        <div class="flex justify-between mb-6">
+                            <h3>Expense Category</h3>
+                            <h3>Total</h3>
+                        </div>
+
+                        <div class="flex justify-between">
+                            @forelse ($expense_category_total as $category => $expense_total)
+                            <p>{{ $category }}</p>
+                            <p>{{ $expense_total }}</p>
+                            @else
+                            <p>No available expenses</p>
+                            @endforelse
+                        </div>
+                    </div>
                 </div>
-
-                <a href="#">Expense Management</a>
-                <div class="flex flex-col ml-3 space-y-2">
-                    <a href="#">Expense Category</a>
-                    <a href="#">Expense</a>
-                </div>
-            </nav>
-
+                <div class="flex-1 border border-black"></div>
+            </div>
         </div>
-        <div class="flex-1 border-2 border-red-700"></div>
     </div>
 
 </x-layouts.app>
