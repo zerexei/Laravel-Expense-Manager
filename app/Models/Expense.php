c3a6469b3bc2c79a84483e3b9e69dc55be8f310a
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-05-17 08:26:12 +0800

    Add Expense ExpenseCategory relationship

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Expense extends Model
 {
     use HasFactory;
+
+    public function categories()
+    {
+        return $this->belongsTo(ExpenseCategory::class);
+    }
 }
--- app/Models/ExpenseCategory.php
+++ app/Models/ExpenseCategory.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class ExpenseCategory extends Model
 {
     use HasFactory;
+
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
 }
