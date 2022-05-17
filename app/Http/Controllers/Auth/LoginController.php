de36a03012f04a60e450b32cd06ca110600b9a6d
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-05-17 09:51:20 +0800

    Create LoginController

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    // https://laravel.com/docs/9.x/authentication#authenticating-users
+
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+}
