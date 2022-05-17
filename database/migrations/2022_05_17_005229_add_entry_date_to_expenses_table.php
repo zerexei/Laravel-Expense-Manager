1f41a7f6bafd29fdfbc73740dd65f151517dfe5d
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-05-17 09:01:45 +0800

    Add entry_date column to expenses table

---
+++ database/migrations/2022_05_17_005229_add_entry_date_to_expenses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('expenses', function (Blueprint $table) {
+            // https://stackoverflow.com/questions/20822159/laravel-migration-with-sqlite-cannot-add-a-not-null-column-with-default-value-n
+            $table->date('entry_date')->nullable()->after('amount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('expenses', function (Blueprint $table) {
+            //
+            $table->dropColumn('entry_date');
+        });
+    }
+};
