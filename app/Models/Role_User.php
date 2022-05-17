e55902dc0ee101d6000d7497d525da301fbd3760
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-05-17 08:09:02 +0800

    Change: Role_User extends Pivot

--- app/Models/Role_User.php
+++ app/Models/Role_User.php
@@ -2,10 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class Role_User extends Model
+class Role_User extends Pivot
 {
-    use HasFactory;
 }
