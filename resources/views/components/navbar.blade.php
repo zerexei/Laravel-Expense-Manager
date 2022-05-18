404c6fc3926bec2653bb4e41db5d9fe13b548977
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-05-18 10:11:43 +0800

    Refactor layouts

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,6 @@
 @if (Route::has('login'))
 <div class="flex justify-end space-x-4 p-6">
+    {{-- AUTH --}}
     @auth
     <h3>Welcome to Expense Manager</h3>
     <button @click="this.$refs.logoutForm.submit()">Logout</button>
@@ -7,6 +8,8 @@
         @method('DELETE')
         @csrf
     </form>
+
+    {{-- GUEST --}}
     @else
     <a href="{{ url('/') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
     <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
